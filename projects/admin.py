e189ba88c3621f40819eca6b8e3b19fce8e8a5af
Rajesh Vallepalli <vallepalliraj@gmail.com>
2021-11-30 08:23:25 -0600

    Created requirements files for the build and updated list display

--- projects/admin.py
+++ projects/admin.py
@@ -5,9 +5,5 @@ from projects.models import Project
 # Register your models here.
 
 class ProjectAdmin(admin.ModelAdmin):
-    # title = forms.CharField(max_length=100)
-    # description = forms.TextField()
-    # technology = forms.CharField(max_length=20)
-    # image = forms.FilePathField(path="/img")
-    fields = ('title','description','technology','image')
+    list_display =['title','description','technology','image']
 admin.site.register(Project,ProjectAdmin)

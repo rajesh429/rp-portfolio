0d001a44144bdc7d1ecb10458c8b19f21d5dfdb7
Rajesh Vallepalli <vallepalliraj@gmail.com>
2021-11-30 21:30:09 -0600

    Added image feild added pillow added finals

--- projects/admin.py
+++ projects/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.forms import forms
-
-from projects.models import Project
+from django.forms import ModelForm
+from .models import Project
 # Register your models here.
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display =['title','description','technology','image']
+    list_display =['title','description','language','image']
+
 admin.site.register(Project,ProjectAdmin)
--- projects/models.py
+++ projects/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-
-# Create your models here.
 from django.urls import reverse
 from django.utils.text import slugify
+from django.forms import ModelForm
 
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="/img")
+    language = models.CharField(max_length=20)
+    # image = models.FilePathField(path="/img")
+    image = models.ImageField(upload_to='img/')
     slug = models.SlugField(default="", blank= True, editable=True, null=False, db_index=True)
 
     def get_absolute_url(self):
@@ -22,3 +22,7 @@ class Project(models.Model):
     def __str__(self):
         return f"{self.title}"
 
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = ['title','description','language','image']
